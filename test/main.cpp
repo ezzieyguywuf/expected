95a9d58273eba057d7788f48f370dfc4321421fa
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-07-09 18:36:03 -0400

    Refactored test
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- test/main.cpp
+++ test/main.cpp
@@ -3,18 +3,32 @@
 #include <string>
 #include <iostream>
 
+struct A
+{
+    int x;
+};
+
+struct B
+{
+    std::string s;
+};
+
 int main()
 {
     using EitherInt = nonstd::expected<int, std::string>;
     EitherInt val(10);
 
-    std::cout << "val.has_value() = " << std::boolalpha
-              << val.has_value() << '\n';
+    if(not val.has_value())
+    {
+        return 1;
+    }
 
     val = EitherInt("error");
 
-    std::cout << "val.has_value() = " << std::boolalpha
-              << val.has_value() << '\n';
+    if (val.has_value())
+    {
+        return 0;
+    }
 
     return 0;
 }
