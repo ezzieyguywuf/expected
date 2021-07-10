daa1c8830d572d28618a35d67ce14950843141ba
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-07-09 22:04:55 -0400

    Added >>= operator to expected class
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- include/ezzieyguywuf/expected.h
+++ include/ezzieyguywuf/expected.h
@@ -1,6 +1,7 @@
 #ifndef EZZIEY_EXPECTED_HEADER
 #define EZZIEY_EXPECTED_HEADER
 
+#include <functional>
 #include <variant>
 
 namespace nonstd
@@ -17,8 +18,15 @@ namespace nonstd
                 return std::holds_alternative<Val>(data);
             }
 
+            template<typename Val2>
+            expected<Val2, Err> operator>>=(std::function<expected<Val2, Err>(const Val&)> f)
+            {
+                return has_value() ? f(std::get<Val>(data)) : *this;
+            }
+
         private:
             std::variant<Val, Err> data;
     };
+
 } // namespace nonstd
 #endif
--- test/main.cpp
+++ test/main.cpp
@@ -13,22 +13,48 @@ struct B
     std::string s;
 };
 
+using EitherAB = nonstd::expected<A, B>;
+
+auto good() -> EitherAB
+{
+    return EitherAB(A(10));
+}
+
+auto goodUsesA(const A&) -> EitherAB
+{
+    return EitherAB(A(20));
+}
+
+auto badUsesA(const A&) -> EitherAB
+{
+    return EitherAB(B("Dang"));
+}
+
+auto bad() -> EitherAB
+{
+    return EitherAB(B("Whoops"));
+}
+
 int main()
 {
     using EitherInt = nonstd::expected<int, std::string>;
     EitherInt val(10);
 
-    if(not val.has_value())
-    {
-        return 1;
-    }
+    if(not val.has_value()) return 1;
 
     val = EitherInt("error");
 
-    if (val.has_value())
-    {
-        return 0;
-    }
+    if (val.has_value()) return 0;
+
+    EitherAB first  = good();
+    EitherAB second = bad();
+
+    if (not first.has_value()) return 1;
+    if (second.has_value()) return 1;
+
+    auto ret = (good() >>= std::function(goodUsesA)) >>= std::function(badUsesA);;
+
+    if (ret.has_value()) return 1;
 
     return 0;
 }
