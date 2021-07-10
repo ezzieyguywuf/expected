8171f0c73f4aa2d173316af0e5c4b3e51eafeac7
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-07-09 23:56:20 -0400

    Updated comments in demo
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- test/main.cpp
+++ test/main.cpp
@@ -41,31 +41,34 @@ auto bad() -> EitherAB
 
 int main()
 {
+    // Demo of c++-style bind operators
+    //
+    // >>= from haskell will be >= here
+    // >>  from haskell will be >  here
+    //
+    // I couldn't match the operators exactly due to precedence and
+    // associativity, which are not definable in c++
     using EitherInt = nonstd::expected<int, std::string>;
-    EitherInt val(10);
 
+    // It should have a value because we made sure it did
+    EitherInt val(10);
     if(not val.has_value()) return 1;
 
+    // This one definitely has an error
     val = EitherInt("error");
-
     if (val.has_value()) return 0;
 
-    EitherAB first  = good();
-    EitherAB second = bad();
-
-    if (not first.has_value()) return 1;
-    if (second.has_value()) return 1;
-
+    //  This should short-circuit on `badUsesA`
     auto ret = good() >= goodUsesA >= badUsesA >= goodUsesA;
-
     if (ret.has_value()) return 1;
 
+    // This sholud go all the way through - notice we can discard results with
+    // operator::>
     ret = good() >= goodUsesA > good;
-
     if (not ret.has_value()) return 1;
 
+    // Finally, this will short-circuit on `bad`, just like the >= operator
     ret = good() >= goodUsesA > good > good > bad > good > good > good;
-
     if (ret.has_value()) return 1;
 
     return 0;
